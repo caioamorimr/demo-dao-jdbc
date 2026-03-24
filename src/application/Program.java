521dd8ece2fe7c1ebc131b3565c3687eeff0c32c
Caio Amorim <caioamorimribeiro@gmail.com>
2026-03-24 12:40:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@email.com", LocalDate.of(2000, 4, 23), 3000.0, department);
+
+        System.out.println(seller);
     }
 }
